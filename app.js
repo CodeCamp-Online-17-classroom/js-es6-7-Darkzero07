b938a7bc81c154b3ca35e2de9faff7e4f79baec7
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-27 15:24:14 +0700

    js lab 151

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+// จงเขียนฟังก์ชัน doubleAndReturnArgs โดยรับพารามิเตอร์มาเป็น Array และตัวเลขกี่ตัวก็ได้ และ return ค่าเป็น Array ที่ประกอบด้วยค่าเดิมในพารามิเตอร์ที่เป็น Array และค่าพารามิเตอร์ที่เป็นตัวเลขคูณสอง
+
+function doubleAndReturnArgs(arr, ...numbers) {
+  const number = numbers.map((num) => num * 2);
+  return [...arr, ...number];
+}
+
+console.log(doubleAndReturnArgs([1, 2, 3], 4, 4));
+// expexted result: [1, 2, 3, 8, 8]
+console.log(doubleAndReturnArgs([2], 10, 4));
+// expexted result: [2, 20, 8]
